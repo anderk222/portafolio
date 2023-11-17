093beaa45a88ff767c452ae4ec3a2fa3d6426148
anderk222 <anderk222@gmail.com>
2023-11-16 21:35:55 -0500

    add social networks

--- src/feature/resume/components/AboutHeader.tsx
+++ src/feature/resume/components/AboutHeader.tsx
@@ -1,23 +1,39 @@
-import { Divider } from "semantic-ui-react";
+import { Button, Divider, Icon } from "semantic-ui-react";
+import { social } from "../../../helpers/about";
 
 const AboutHeader = () => {
 
     return (
         <>
-            <section>
+            <section className="flex justify-between items-center" >
+                <div>
                 <h1 className="text-5xl" >
                     Anderson Macias
                 </h1>
                 <p className="text-xs" >Software Developer</p>
 
                 <p>Appacionate software Developer, main tools are SpringBoot & Angular 🎆</p>
-
-                <Divider inverted />
+                </div>
+                <div>
+                <Button
+                onClick={()=> outNav(social.linkedin)}
+                circular color='linkedin' size="mini" icon='linkedin' />
+                <Button
+                onClick={()=> outNav(social.github)}
+                circular color='black' size="mini" icon='github' />
+                </div>
             </section>
+            <Divider inverted />
         </>
-    )
+    );
 
 };
 
+function outNav(url : string){
+
+    window.open(url, '_blank')
+
+
+}
 
 export default AboutHeader;
---
+++ src/helpers/about.ts
@@ -0,0 +1,5 @@
+export const social = {
+    linkedin : 'https://www.linkedin.com/in/anderson-macias-29abaa223/',
+    github : 'https://github.com/anderk222/' 
+
+}
