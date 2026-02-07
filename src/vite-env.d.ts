1dd2735409336121fd32cdbf459e49a600d6e436
Anderson Macias <anderk222@gmail.com>
2026-02-06 23:31:34 -0500

    feat: crear variable de entorno para imagen de perfil fallback

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
     readonly VITE_APP_TITLE: string
-    readonly VITE_API_URL: string;
+    readonly VITE_API_URL: string
+    readonly VITE_FALLCBAK_PROFILE_IMG: string
     
   }
   
